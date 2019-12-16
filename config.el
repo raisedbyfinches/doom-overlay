92de55d9544003974f46a501e5a03f3dc57ae16e
karetsu <raisedbyfinches@hotmail.co.uk>
2019-12-16 10:34:10 +0000

    feat: swapped out python repl for nix-shell python repl

--- config.el
+++ config.el
@@ -2,13 +2,14 @@
 
 ;; Place your private configuration here
 
-;; -- Appearance ----------------------------------------------------------------
+;; -- Appearance ---------------------------------------------------------------
 (setq doom-theme 'doom-laserwave)
 (setq doom-font (font-spec :family "Iosevka Custom" :size 14))
+(setq doom-big-font (font-spec :family "Iosevka Custom" :size 32))
 ;;(setq doom-font (font-spec :family "mononoki" :size 14))
 
 
-;; -- Haskell -------------------------------------------------------------------
+;; -- Haskell ------------------------------------------------------------------
 (after! haskell
   (setq haskell-process-type 'cabal-new-repl)
   (setq haskell-process-wrapper-function
@@ -17,7 +18,7 @@
                   (list (mapconcat 'identity argv " "))))))
 
 
-;; -- Org -----------------------------------------------------------------------
+;; -- Org ----------------------------------------------------------------------
 (after! org
   (add-to-list 'org-modules 'org-habit t))
 (setq org-directory "~/.org/"
@@ -29,5 +30,16 @@
       org-bullets-bullet-list '(""))
 
 
-;; -- Text ----------------------------------------------------------------------
+;; -- Python -------------------------------------------------------------------
+(after! python
+  (defun open-ipython-repl ()
+    (interactive)
+    (pop-to-buffer
+     (process-buffer
+      (run-python "nix-shell --command python3" nil t))))
+  (set-repl-handler! 'python-mode #'open-ipython-repl :persist t))
+
+
+
+;; -- Text ---------------------------------------------------------------------
 (add-hook! text-mode #'turn-on-visual-line-mode)
