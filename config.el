aef5747c6fec60a975e9196851aca4a484fdb857
raisedbyfinches <20670199+raisedbyfinches@users.noreply.github.com>
2025-06-30 14:56:01 +0100

    feat: add copilot integration into emacs

--- config.el
+++ config.el
@@ -203,3 +203,14 @@
   `(company-box-background :background ,(doom-darken (doom-color 'bg) 0.1))
   `(ein:cell-input-area :background ,(doom-color 'bg-alt) )
   )
+
+;; -- assistant -----------------------------------------------------------------
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+
+(map! :leader :desc "Copilot menu" "d" #'copilot-chat-transient)
--- packages.el
+++ packages.el
@@ -15,3 +15,10 @@
 
 ;; replace LaTeX with xenops
 ;; (package! xenops)
+
+;; testing out copilot
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+
+(package! copilot-chat
+  :recipe (:host github :repo "chep/copilot-chat.el" :files ("*.el")))
